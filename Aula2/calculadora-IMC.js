bf3167b39b073a9dcfa0c767b20fb08e2a26cde9
Laboratorios de Informatica CEULP <labins.pmw@ensino.ulbra.net>
2025-08-06 21:44:18 -0300

    ajeitando o obvio

--- Aula2/calculadora-IMC.js
+++ Aula2/calculadora-IMC.js
@@ -1,3 +1,11 @@
 const peso  = 80;
 const altura = 1.70;
 console.log('IMC:' + peso / altura ** 2)
+function classificarIMC(imc) {
+    if (imc < 18.5) return "Abaixo do peso";
+    else if (imc < 25) return "Peso normal";
+    else if (imc < 30) return "Sobrepeso";
+    else if (imc < 35) return "Obesidade grau 1";
+    else if (imc < 40) return "Obesidade grau 2";
+    else return "Obesidade grau 3 (mórbida)";
+}
